6fc72c70db6b203a9f29bf5b8236c7c2288be6ec
Franklin Harding <32021905+fharding1@users.noreply.github.com>
2017-11-22 21:54:59 -0800

    Added 2s delay after starting PQ to scripts test.sh and test.bat (#68)

--- test.bat
+++ test.bat
@@ -1,4 +1,5 @@
 docker-compose -f compose-testing.yml down
 docker-compose -f compose-testing.yml build
 docker-compose -f compose-testing.yml up -d postgres
+timeout 3
 docker-compose -f compose-testing.yml run go
--- test.sh
+++ test.sh
@@ -3,4 +3,5 @@
 docker-compose -f compose-testing.yml down
 docker-compose -f compose-testing.yml build
 docker-compose -f compose-testing.yml up -d postgres
+sleep 2
 docker-compose -f compose-testing.yml run go
