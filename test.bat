629a259e100a668c84ac61f89ec23171c9c521d5
Franklin Harding <32021905+fharding1@users.noreply.github.com>
2017-11-22 23:00:03 -0800

    Made test.sh stop postgres after running (#69)

--- test.bat
+++
@@ -1,5 +0,0 @@
-docker-compose -f compose-testing.yml down
-docker-compose -f compose-testing.yml build
-docker-compose -f compose-testing.yml up -d postgres
-timeout 3
-docker-compose -f compose-testing.yml run go
--- test.sh
+++ test.sh
@@ -3,5 +3,7 @@
 docker-compose -f compose-testing.yml down
 docker-compose -f compose-testing.yml build
 docker-compose -f compose-testing.yml up -d postgres
-sleep 2
+sleep 3
 docker-compose -f compose-testing.yml run go
+docker-compose -f compose-testing.yml down
+
